a479a8d481d4739ed11288a88feb1b65e96f6c63
Sam Gordon <gordo608@umn.edu>
2017-02-05 21:33:16 -0600

    first launch samgordon.design

--- app.js
+++ app.js
@@ -6,34 +6,34 @@ $('a.continue').click( function() {
     $(this).parent().next().next().addClass('exists');
 } );
 $('a.showall').click( function() {
-	$('.portfolio-wrap').addClass('exists');
+	$('.portfolio').addClass('exists');
 	$('.outro').addClass('exists');
 } );
 
-// BG IMAGE
+// // BG IMAGE
 
-var divs = $('.headimg'),
-	$window = $(window);
+// var divs = $('.headimg'),
+// 	$window = $(window);
 
-var img = $('.headimg img');
+// var img = $('.headimg img');
 
-$window.on('scroll', function() {
-  var st = $(this).scrollTop(),
-    pixs = 100 - (st / 5);
-  if (st < 500) {
-    img.css ({
-      '-webkit-filter': 'blur('+pixs+'px)'
-    })
-  } else if ( st >= 500 && st < 2000) {
-    img.css ({
-      '-webkit-filter': 'blur(0px)',
-    })
-    divs.css ({
-      'opacity': '1',
-    })
-  } else {
-    divs.css ({
-      'opacity': '0'
-    })
-  }
-});
+// $window.on('scroll', function() {
+//   var st = $(this).scrollTop(),
+//     pixs = 100 - (st / 5);
+//   if (st < 500) {
+//     img.css ({
+//       '-webkit-filter': 'blur('+pixs+'px)'
+//     })
+//   } else if ( st >= 500 && st < 2000) {
+//     img.css ({
+//       '-webkit-filter': 'blur(0px)',
+//     })
+//     divs.css ({
+//       'opacity': '1',
+//     })
+//   } else {
+//     divs.css ({
+//       'opacity': '0'
+//     })
+//   }
+// });
